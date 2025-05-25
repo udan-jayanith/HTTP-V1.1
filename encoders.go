46445f1930772fe14c67a8004cf974b40c4419c8
Udan Jayanith Jayakody <j.udanjayanith@gmail.com>
2025-05-25 14:14:03 +0530

    update encoders.go

--- encoders.go
+++ encoders.go
@@ -13,21 +13,25 @@ type HTTPResponseLine struct {
 	StatusCodeMessage string
 }
 
+//SetStatusCode sets the HTTP status code.
 func (hr *HTTPResponseLine) SetStatusCode(statusCode int) {
 	hr.StatusCode = statusCode
 	hr.StatusCodeMessage = http.StatusText(statusCode)
 }
 
+//EncodeRequestLine return the current responses request line.
 func (hr *HTTPResponseLine) EncodeRequestLine() []byte {
 	return []byte(fmt.Sprintf("HTTP/1.1 %v %s", hr.StatusCode, hr.StatusCodeMessage))
 }
 
+//HTTPResponse is used when sending a response to a HTTP request.
 type HTTPResponse struct {
 	ResponseLine HTTPResponseLine
 	header       []byte
 	body         []byte
 }
 
+//SetHeaderLine header add header linde to the HTTP header as field: value.
 func (hr *HTTPResponse) SetHeaderLine(field, value string) {
 	if hr.header == nil {
 		hr.header = []byte(fmt.Sprintf("%s: %s\n", field, value))
@@ -37,6 +41,7 @@ func (hr *HTTPResponse) SetHeaderLine(field, value string) {
 	hr.header = append(hr.header, []byte(fmt.Sprintf("%s: %s\r\n", field, value))...)
 }
 
+//Write writes to the HTTPResponse body.
 func (hr *HTTPResponse) Write(b []byte) (int, error) {
 	if hr.body == nil {
 		hr.body = b
@@ -47,17 +52,18 @@ func (hr *HTTPResponse) Write(b []byte) (int, error) {
 	return len(b), nil
 }
 
+//WriteAsJson set body as value pointed by v.
 func (hr *HTTPResponse) WriteAsJson(v any) error {
 	jsonByt, err := jsoniter.Marshal(v)
 	if err != nil {
 		return err
 	}
-
 	hr.body = jsonByt
 	hr.SetHeaderLine("Content-Type", "application/json")
 	return err
 }
 
+//EncodeHTTPResponse return HTTP response.
 func (hr *HTTPResponse) EncodeHTTPResponse() []byte {
 	res := []byte{}
 	res = append(res, hr.ResponseLine.EncodeRequestLine()...)
